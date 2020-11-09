f33d44444d5546cd2cc44f6da73c69434b358569
Laurent Capello <laurent.capello@gmail.com>
2020-11-09 12:58:50 +0100

    add two input, remove default html arrows

--- src/components/TimeForm/TimeForm.js
+++ src/components/TimeForm/TimeForm.js
@@ -3,8 +3,21 @@ import "./TimeForm.css"
 
 function TimeForm(){
   return (
-    <div>TimeForm</div>
+    <div id="time-form-div">
+      <input id="time-form-hours" type="number" min="00" max="23"></input>
+      <input id="time-form-minutes" type="number" min="00" max="59"></input>
+    </div>
   )
 }
+
+//fare un text input che deve avere il formato HH:MM
+  //è un component fatto di input-numero string(:) input-numero
+  //HH deve andare da 00 a 23
+  //MM deve andare da 00 a 60
+//ad avvio applicazione salvare in state la data attuale ed estrarre in una variabile il numero
+ //di minuti passati dall'inizio del giorno moltiplicando getHour per il numero corretto di minuti
+//quando utente modifica input salvare nello state il numero di minuti passati dall'inizio del giorno
+  //
+//EXTRA: ricontrollare ora locale ogni volta che modifica
 
 export default TimeForm
