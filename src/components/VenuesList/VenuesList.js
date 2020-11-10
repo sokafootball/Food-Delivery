c5b016b92466211cfca05cc4c4d56fca410102d9
Laurent Capello <laurent.capello@gmail.com>
2020-11-10 16:26:32 +0100

    add render of card in VenuesList

--- src/components/VenuesList/VenuesList.js
+++ src/components/VenuesList/VenuesList.js
@@ -4,8 +4,18 @@ import VenueCard from "../VenueCard/VenueCard"
 
 function VenuesList(props){
 
+  const venueCards = props.availableVenues.map((venue, idx) => {
+    <VenueCard
+      key={idx}
+      name={venue.name}
+      description={venue.description}
+      cover={venue.cover}
+      category={venue.category}
+    />
+  })
+
   return (
-    <div>venues</div>
+    <div>{venueCards}</div>
   )
 }
 
